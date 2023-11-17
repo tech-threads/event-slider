28e6030d8d3a090e45f369baa8ed29c4a16df6b1
Brent Dalling <bdalling@sparkfish.com>
2023-11-17 10:58:56 -0700

    Init commit

---
+++ script.js
@@ -0,0 +1,305 @@
+import moment from "https://esm.sh/moment";
+import YouTubePlayer from "https://esm.sh/youtube-player";
+
+let activeVideo = null;
+
+const rampVolume = (targetPlayer, volume, direction, callback = () => {}) => { 
+  let interval = setInterval(async () => {
+    let currentVolume = await targetPlayer.getVolume();
+    if (currentVolume === volume) {
+      clearInterval(interval);
+      callback();
+    } else {
+      targetPlayer.setVolume(currentVolume + (direction === "up" ? 1 : -1))
+    }
+  }, 100)
+};
+
+const minutesToMilliseconds = minutes => minutes * 60 * 1000;
+
+// HH:mm:ss Countdown
+const startCountDown = (eventEndDateTime, eventName) => {
+  const wrapper = document.getElementById(eventName);
+  const hour = wrapper.querySelector(".hour");
+  const minute = wrapper.querySelector(".minute");
+  const second = wrapper.querySelector(".second");
+
+  let interval = setInterval(() => {
+    let now = moment();
+
+    let eventEnd = moment(eventEndDateTime);
+    let duration = moment.duration(eventEnd.diff(now));
+
+    let hours = duration.asHours().toFixed(2).padStart(2, '0');
+    let minutes = duration.minutes().toString().padStart(2, '0');
+    let seconds = duration.seconds().toString().padStart(2, '0');
+
+    hour.innerHTML = hours.slice(0, 2);
+    minute.innerHTML = minutes;
+    second.innerHTML = seconds;
+  }, 1000);
+}
+
+const srcSponsors = [
+  {
+    name: "Vasion",
+    logo:
+      "https://southernutahcodecamp.com/wp-content/uploads/2013/11/vasion_logo.png",
+    level: "platinum"
+  },
+  {
+    name: "Zonos",
+    logo:
+      "https://southernutahcodecamp.com/wp-content/uploads/2015/11/zonos-300x300-1.png",
+    level: "platinum"
+  },
+  {
+    name: "St. George Academy",
+    logo:
+      "https://southernutahcodecamp.com/wp-content/uploads/2022/11/sga-300x300-1.png",
+    level: "platinum"
+  },
+  {
+    name: "St. George Chamber of Commerce",
+    logo:
+      "https://southernutahcodecamp.com/wp-content/uploads/2020/11/stg-chamber-300x300-1.jpg",
+    level: "platinum"
+  },
+  {
+    name: "Rural Online Initiative",
+    logo:
+      "https://southernutahcodecamp.com/wp-content/uploads/2021/11/usu_roi_logo.png",
+    level: "platinum"
+  },
+  {
+    name: "InfoWest",
+    logo:
+      "https://southernutahcodecamp.com/wp-content/uploads/2018/09/infowest.png",
+    level: "platinum"
+  }
+];
+const sponsorList = [
+  ...srcSponsors,
+  {
+    logo: `./frame.png`
+  }
+];
+const defaultIframe =
+  "https://www.youtube.com/embed/B-0wgmKDxho?si=QN79JaPR_YMoIBUr";
+
+async function getConfig() {
+  const url =
+    "https://6555573184b36e3a431db63e.mockapi.io/config";
+
+  const getConfigRequest = new Request(url + "?time=" + new Date().getTime(), {
+    method: "GET",
+    rejectUnauthorized: false,
+    insecureHTTPParser: true,
+    mode: "cors",
+    cache: "no-store"
+  });
+
+  const response = await fetch(getConfigRequest).then((resp) => resp.json());
+  // Check if the fetch was successful
+  if (!response) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+
+  const config = response[0];
+  return config;
+}
+
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+const start = async function () {
+  let config = {};
+
+  var musicPlayer = YouTubePlayer("videomusic", {
+    playerVars: {
+      autoplay: 1,
+      controls: 0,
+      mute: 1, 
+      listType:'playlist',
+      list: 'PLhuOfWAQ8ocgRk7384XFcZSb56ZlCfUwa'
+    }
+  })
+
+  musicPlayer.on('onError', (event) => {
+    musicPlayer.nextVideo()
+  })
+
+  var player = YouTubePlayer("video", {
+    playerVars: {
+      autoplay: 1,
+      controls: 0,
+      mute: 1
+    }
+  })
+
+  // musicPlayer.loadVideoByUrl(defaultIframe)
+  // activeVideo = defaultIframe
+  musicPlayer.unMute()
+  player.unMute()
+  musicPlayer.setVolume(20)
+  rampVolume(musicPlayer, 20, "up")
+
+  try {
+    config = await getConfig();
+    startCountDown(config['event-end-times'][0]['day-camp'], 'countdown-daycamp');
+    startCountDown(config['event-end-times'][0]['cc-classic'], 'countdown-classic');
+  } catch (e) {
+    console.error(e);
+  }
+
+  var scroller = document.getElementById("scroller");
+
+  if (scroller) {
+    var block = scroller.querySelector(".block");
+    scroller.querySelectorAll(".box").forEach((block) => {
+      block.setAttribute(
+        "style",
+        "height: " + getRandomArbitrary(30, 80) + "%;"
+      );
+    });
+    if (block) {
+      var sponsorIndex = 0;
+
+      for (let i = 0; i < 100; i++) {
+        var newBlock = block.cloneNode(true);
+        newBlock.querySelectorAll(".box").forEach((block) => {
+          block.setAttribute(
+            "style",
+            "height: " + getRandomArbitrary(30, 80) + "%;"
+          );
+        });
+
+        if (!sponsorList[sponsorIndex]) continue;
+
+        var sponsor = sponsorList[sponsorIndex];
+
+        newBlock
+          .querySelector(".content")
+          .setAttribute(
+            "style",
+            "background-image: url('" + sponsor.logo + "');"
+          );
+
+        if (sponsorIndex >= sponsorList.length - 1) {
+          sponsorIndex = 0;
+        } else {
+          sponsorIndex++;
+        }
+        scroller.appendChild(newBlock);
+      }
+    }
+
+    var configUpdateLoop = setInterval(async () => {
+      config = await getConfig();
+    }, 10000);
+
+    var configLoop = setInterval(async () => {
+      const clock = document.querySelector(".clock");
+
+      const hour = clock.querySelector(".hour");
+      const minute = clock.querySelector(".minute");
+      const ampm = clock.querySelector(".ampm");
+
+      const now = moment();
+
+      hour.innerHTML = now.format("hh");
+      minute.innerHTML = now.format("mm");
+      ampm.innerHTML = now.format("A");
+
+      if (config['wifi-information']) {
+        const networkName = document.querySelector('.network-name')
+        const networkPass = document.querySelector('.network-password')
+
+        networkName.innerHTML = config['wifi-information']['network-name']
+        networkPass.innerHTML = config['wifi-information']['network-password']
+      }
+      if (config.announcements) {
+        config.announcements.forEach((announcement) => {
+          let showAt = moment(announcement["show-at"], 'YYYY-MM-DD hh:mm:ss');
+          let hideAt = moment(announcement["hide-at"], 'YYYY-MM-DD hh:mm:ss');
+          let now = moment();
+
+          // show if showAt is before now and hideAt is after now and hideAt is after showAt
+          if (
+            showAt.isSameOrBefore(now) && 
+            showAt.isSameOrBefore(hideAt) &&
+            !hideAt.isBefore(now)) {
+            // Show it
+            switch (announcement["type"]) {
+              case "video":
+                if (
+                  activeVideo !==
+                  announcement["src"]
+                ) {
+                  const videoPlayer = document.getElementById('video-player')
+
+                  rampVolume(musicPlayer, 0, "down", () => {
+                    videoPlayer.classList.add('open')
+                    player.loadVideoByUrl(announcement["src"])
+                    rampVolume(player, 100, "up")
+                  })
+                  
+                  activeVideo = announcement["src"]
+
+                  if (announcement['duration']) {
+                    setTimeout(
+                      () => {
+                        rampVolume(player, 0, "down", () => {
+                          videoPlayer.classList.remove('open')
+                          rampVolume(musicPlayer, 20, "up")
+                        })
+                        activeVideo = defaultIframe
+                      },
+                      minutesToMilliseconds(announcement['duration']) // update to grab from config after work
+                    );
+                  }
+                }
+                break;
+
+              case "text":
+                let text = document.getElementById("text-announcement");
+
+                text.innerHTML = announcement.src;
+
+                if (announcement['duration']) {
+                  setTimeout(
+                    () => {
+                      text.innerHTML = '';
+                    },
+                    minutesToMilliseconds(announcement['duration']) // update to grab from config after work
+                  );
+                }
+                break;
+            }
+          }
+        });
+      }
+    }, 1000);
+
+    var loop = setInterval(() => {
+      window.requestAnimationFrame(() => {
+        scroller.scrollLeft += 1;
+
+        // reset when we reach the end of the scroller
+        if (scroller.scrollLeft >= scroller.scrollWidth - 1920) {
+          scroller.scrollLeft -= scroller.scrollWidth / 2;
+          scroller.innerHTML += scroller.children
+        }
+      });
+    }, 60);
+  }
+};
+
+window.start = start;
+
+// Needs:
+// assets for buildings
+// -- music
+// countdown
+// -- links (fetch from github repo?)
